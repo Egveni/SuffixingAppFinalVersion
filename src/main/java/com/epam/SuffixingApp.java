8ad76c147d0f5bc9a39bcbe3da41430b5d4049ad
Evgenii Goncharov <Eugeni.goncharov@gmail.com>
2022-10-25 13:13:45 +0400

    Final corrections

--- src/main/java/com/epam/SuffixingApp.java
+++ src/main/java/com/epam/SuffixingApp.java
@@ -11,16 +11,17 @@ import java.util.logging.Logger;
 
 public class SuffixingApp {
     private static final Logger logger = Logger.getLogger(SuffixingApp.class.getName());
-    private static final String CONFIG_FILE_PATH = "src/main/resources/config.properties";
-    private static final Properties property = new Properties();
 
     public static void main(String[] args) throws IOException {
-        renameFiles();
+        String configFile = args[0];
+        Properties property = new Properties();
+        renameFiles(configFile, property);
     }
-
-    public static void renameFiles() throws IOException {
-        String filesSource = getFilesSource();
-        if (filesSource == null) return;
+    public static void renameFiles(String configFile, Properties property) throws IOException {
+        String filesSource = getFilesSource(configFile, property);
+        if (filesSource == null) {
+            return;
+        }
         String[] files = filesSource.split(":");
         for (String originalFilePath : files) {
             File originalFile = new File(originalFilePath);
@@ -28,36 +29,36 @@ public class SuffixingApp {
                 logger.log(Level.SEVERE, "No such file: {0}", originalFilePath);
             } else {
                 String suffix = property.getProperty("suffix");
-                if (suffix.equalsIgnoreCase("")) {
+                if (suffix == null || suffix.isEmpty()) {
                     logger.log(Level.SEVERE, "No suffix is configured");
-                    break;
+                    return;
                 } else {
                     File renamedFiles = new File(originalFilePath.substring(0, originalFilePath.lastIndexOf(".")) + suffix + originalFilePath.substring(originalFilePath.lastIndexOf(".")));
-                    copyOrMoveFile(originalFilePath, originalFile, renamedFiles);
+                    copyOrMoveFile(originalFilePath, originalFile, renamedFiles, property);
                 }
             }
         }
     }
-
-    private static void copyOrMoveFile(String originalFilePath, File originalFile, File renamedFiles) throws IOException {
+    private static void copyOrMoveFile(String originalFilePath, File originalFile, File renamedFiles, Properties property) throws IOException {
         String mode = property.getProperty("mode");
+        String originFilePath = originalFile.getPath().replace("\\", "/");
+        String renamedFilePath = renamedFiles.getPath().replace("\\", "/");
         if (mode.equalsIgnoreCase("copy")) {
             Files.copy(Path.of(originalFilePath), Path.of(String.valueOf(renamedFiles)));
-            logger.log(Level.INFO, originalFile.getAbsolutePath() + "->" + renamedFiles.getAbsolutePath());
+            logger.log(Level.INFO, originFilePath + " -> " + renamedFilePath);
         } else if (mode.equalsIgnoreCase("move") && originalFile.renameTo(renamedFiles)) {
-            logger.log(Level.INFO, originalFile.getAbsolutePath() + "=>" + renamedFiles.getAbsolutePath());
+            logger.log(Level.INFO, originFilePath + " => " + renamedFilePath);
         } else {
             logger.log(Level.SEVERE, "Mode is not recognized: {0}", mode);
         }
     }
-
-    private static String getFilesSource() throws IOException {
-        FileInputStream propertiesInput = new FileInputStream(CONFIG_FILE_PATH);
-        property.load(propertiesInput);
+    private static String getFilesSource(String configFile, Properties property) throws IOException {
+        try (FileInputStream propertiesInput = new FileInputStream(configFile)) {
+            property.load(propertiesInput);
+        }
         String filesSource = property.getProperty("files");
-        if (filesSource.equalsIgnoreCase("")) {
+        if (filesSource == null || filesSource.isEmpty()) {
             logger.log(Level.WARNING, "No files are configured to be copied/moved");
-            return null;
         }
         return filesSource;
     }
